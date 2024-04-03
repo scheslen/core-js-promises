23b39d14bd60107297adb40c7b941ce904a36952
scheslen <antgalin@mail.ru>
2024-04-03 20:35:40 +0300

    feat: implement tasaks

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,11 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  return new Promise((res, rej) => {
+    if (number >= 0) res();
+    else rej();
+  });
 }
 
 /**
